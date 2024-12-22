3c7b0e9e36556d5b3a9fcd687a79bfceae815eca
Богдан Колганов <kolganov_25@mail.ru>
2024-12-23 03:22:09 +0700

    Добавьте файлы проекта.

---
+++ LifeSafetyBot/Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace LifeSafetyBot
+{
+    public class Program
+    {
+        
+        private static TelegramBotClient bot;
+        private static readonly string token = "7664998270:AAHgI13c9OE4X8UZimjnLqNzVXv53gGeJyc"; // Замените на свой токен
+        private static string answer;
+        private static bool WaitForAnswer;
+
+        public static void Main(string[] args)
+        {
+            bot = new TelegramBotClient(token);
+            
+            bot.OnError += OnError;
+            bot.OnMessage += OnMessage;
+            bot.OnUpdate += OnUpdate;
+
+            //Console.WriteLine("Нажмите любую клавишу для выхода.");
+            Console.ReadKey();
+
+        }
+
+        async static void StartTesting(Message msg)
+        {
+            WaitForAnswer = false;
+            var inlineMarkup = new InlineKeyboardMarkup();
+            inlineMarkup.AddButton("Начать");
+            await bot.SendMessage(msg.Chat,
+                        "<b><u>Важная информация</u></b>:\n" +
+                        "Все вопросы требуют <b>НЕСКОЛЬКО</b> вариантов ответов. Для того, чтобы выбрать вариант ответа, " +
+                        "нужно нажать на кнопку с этим вариантом под вопросом. Когда вы выбрали варианты ответов, нужно нажать " +
+                        "на кнопку <b>Отправить</b> в самом низу. Удачи!",
+                        parseMode: ParseMode.Html, replyMarkup: inlineMarkup);
+            WaitForAnswer = true;
+            while (WaitForAnswer)
+            {
+                await Task.Delay(100);
+            }
+
+
+            bot.OnMessage -= OnMessage;
+            WaitForAnswer = false;
+            Test test = new Test();
+            List<MedicalKit> kits = MedicalKitExtention.AllMedicalKits();
+            inlineMarkup = new InlineKeyboardMarkup();
+            for (int i = 0; i < kits.Count; i++)
+            {
+                if (i % 3 == 0 && i != 0) inlineMarkup.AddNewRow();
+                inlineMarkup.AddButton(kits[i].GetDescription());
+            }
+            inlineMarkup.AddNewRow();
+            inlineMarkup.AddButton("Ответить");
+
+            string question = test.GetQuestion();
+            while (true) 
+            {
+                answer = "";
+                if (question == null) break;
+                List<MedicalKit> answers = new List<MedicalKit>();
+                await bot.SendMessage(msg.Chat, question);
+                await bot.SendMessage(msg.Chat, "Окажите первую помощь пострадавшему, используя предметы из аптечки:", replyMarkup: inlineMarkup);
+
+                while (answer != "Ответить")
+                {
+                    WaitForAnswer = true;
+                    while (WaitForAnswer)
+                    {
+                        await Task.Delay(100);
+                    }
+                    if (answer != "Ответить" && !answers.Contains(answer.ToMedicalKit()))
+                    {
+                        answers.Add(answer.ToMedicalKit());
+                    }
+                }
+
+                await bot.SendMessage(msg.Chat, test.CheckAnswers(answers));
+
+                question = test.GetQuestion();
+            }
+            await bot.SendMessage(msg.Chat, test.GetResult());
+            bot.OnMessage += OnMessage;
+        }
+
+        async static Task OnError(Exception exception, HandleErrorSource source)
+        {
+            Console.WriteLine(exception);
+        }
+
+        async static Task OnMessage(Message msg, UpdateType type)
+        {
+            if (msg.Text is not { } text)
+                Console.WriteLine($"Received a message of type {msg.Type}");
+            else if (text.StartsWith('/'.ToString()))
+            {
+                var space = text.IndexOf(' ');
+                if (space < 0) space = text.Length;
+                var command = text[..space].ToLower();
+                await OnCommand(command, text[space..].TrimStart(), msg);
+            }
+            else
+                await OnTextMessage(msg);
+        }
+
+        async static Task OnTextMessage(Message msg) // received a text message that is not a command
+        {
+            Console.WriteLine($"Received text '{msg.Text}' in {msg.Chat}");
+            await OnCommand("/start", "", msg); // for now we redirect to command /start
+        }
+
+        async static Task OnCommand(string command, string args, Message msg)
+        {
+            Console.WriteLine($"Received command: {command} {args}");
+            switch (command)
+            {
+                case "/start":
+                    await bot.SendMessage(msg.Chat,
+                        "<b><u>Меню</u></b>:\n"+
+                        "/start_test - Начать тест\n",
+                        parseMode: ParseMode.Html);
+                    break;
+                case "/start_test":
+                    StartTesting(msg);
+                    break;
+            }
+        }
+
+        async static Task OnUpdate(Update update)
+        {
+            switch (update)
+            {
+                case { CallbackQuery: { } callbackQuery }: await OnCallbackQuery(callbackQuery); break;
+                default: Console.WriteLine($"Received unhandled update {update.Type}"); break;
+            };
+        }
+
+        async static Task OnCallbackQuery(CallbackQuery callbackQuery)
+        {
+            await bot.AnswerCallbackQuery(callbackQuery.Id, $"Вы выбрали {callbackQuery.Data}");
+            answer = callbackQuery.Data;
+            WaitForAnswer = false;
+            if(answer == "Ответить" || answer == "Начать")
+                await RemoveKeyboard(bot, callbackQuery.Message.Chat.Id, callbackQuery.Message.MessageId);
+        }
+
+        private static async Task RemoveKeyboard(ITelegramBotClient botClient, long chatId, int messageId)
+        {
+            try
+            {
+                await botClient.EditMessageReplyMarkup(
+                     chatId: chatId,
+                    messageId: messageId,
+                     replyMarkup: null); // Устанавливаем null для удаления клавиатуры
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error removing keyboard: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ LifeSafetyBot/Test.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace LifeSafetyBot
+{
+    public enum MedicalKit
+    {
+        Gloves = 0, Mask, Tourniquet, BandAid, IVL, Napkins, Scissors, SterileBandage, NonSterileBandage, None
+    }
+
+    public static class MedicalKitExtention
+    {
+        public static string GetDescription(this MedicalKit medicalKit)
+        {
+            return medicalKit switch
+            {
+                MedicalKit.Gloves => "Перчатки медицинские",
+                MedicalKit.Mask => "Маска медицинская",
+                MedicalKit.Tourniquet => "Жгут кровоостанавливающий",
+                MedicalKit.BandAid => "Лейкопластырь бактериальный",
+                MedicalKit.IVL => "Устройство для ИВЛ",
+                MedicalKit.Napkins => "Салфетки марлевые стерильные",
+                MedicalKit.Scissors => "Ножницы для разрезания повязок",
+                MedicalKit.SterileBandage => "Бинт марлевый стерильный",
+                MedicalKit.NonSterileBandage => "Бинт марлевый нестерильный",
+                MedicalKit.None => null,
+                _ => null,
+            };
+        }
+        public static MedicalKit ToMedicalKit(this string str)
+        {
+            List<MedicalKit> kits = AllMedicalKits();
+            for (int i = 0; i < kits.Count; i++)
+            {
+                if(str.ToLower() == kits[i].GetDescription().ToLower())
+                {
+                    return kits[i];
+                }
+            }
+            return MedicalKit.None;
+        }
+        public static List<MedicalKit> AllMedicalKits()
+        {
+            int x = 0;
+            var res = new List<MedicalKit>();
+            while (((MedicalKit)x).GetDescription() != null)
+            {
+                res.Add(((MedicalKit)x));
+                x++;
+            }
+            return res;
+        }
+    }
+
+    public class Test
+    {
+        private const string _correct = "Успех!\nВы верно определили предметы медицинского назначения при оказании помощи пострадавшему!";
+        private const string _incorrect = "Неудача!\nВы неверно определили предметы медицинского назначения при оказании помощи пострадавшему!";
+        private readonly string _path;
+        private List<List<MedicalKit>> _correctAnswers;
+        private Queue<int> _randomList;
+        private int _currentQuestionIndex;
+        private List<string> _questions;
+        private List<string> _wrongAnswers;
+        private int _countOfCorrectAnswers;
+        
+        public int CountOfQuestions { get; }
+
+
+        public Test()
+        {
+            _path = AppContext.BaseDirectory;
+            _path = _path.Replace("\\bin\\Debug", "");
+            _path += "TextFiles";
+
+            if (!File.Exists(_path + "\\Questions.txt")) throw new NullReferenceException();
+            _questions = File.ReadLines(_path + "\\Questions.txt").ToList();
+
+            if (!File.Exists(_path + "\\WrongAnswers.txt")) throw new NullReferenceException();
+            _wrongAnswers = File.ReadLines(_path + "\\WrongAnswers.txt").ToList();
+
+            List<string> answers = new List<string>();
+            if (!File.Exists(_path + "\\CorrectAnswers.txt"))
+                throw new NullReferenceException();
+            else
+            {
+                answers = File.ReadLines(_path + "\\CorrectAnswers.txt").ToList();
+                _correctAnswers = new List<List<MedicalKit>>();
+
+                foreach (var item in answers)
+                {
+                    _correctAnswers.Add(new List<MedicalKit>());
+                    string s = new(item.Where(Char.IsDigit).ToArray());
+                    for (int i = 0; i < s.Length; i++)
+                    {
+                        int x = s[i] - '0';
+                        MedicalKit kit = (MedicalKit)x;
+                        _correctAnswers.Last().Add(kit);
+                    }
+                }
+            }
+
+            CountOfQuestions = _questions.Count;
+            _countOfCorrectAnswers = 0;
+
+            List<int> random = new List<int>();
+            for (int i = 0; i < CountOfQuestions; i++)
+            {
+                random.Add(i);
+            }
+            RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
+            int n = random.Count;
+            while (n > 1)
+            {
+                byte[] box = new byte[1];
+                do provider.GetBytes(box);
+                while (!(box[0] < n * (byte.MaxValue / n)));
+                int k = box[0] % n;
+                n--;
+                (random[n], random[k]) = (random[k], random[n]);
+            }
+            _randomList = new Queue<int>(random);
+
+            _currentQuestionIndex = -1;
+
+            
+        }
+
+        public string GetQuestion()
+        {
+            if (_randomList.Count <= 0)
+            {
+                _currentQuestionIndex = -1;
+                return null;
+            }
+            _currentQuestionIndex = _randomList.Dequeue();
+            return _questions[_currentQuestionIndex];
+        }
+
+        public string CheckAnswers(List<MedicalKit> userAnswers)
+        {
+            if (_currentQuestionIndex == -1) return null;
+            if (userAnswers.Count == _correctAnswers[_currentQuestionIndex].Count)
+            {
+                foreach (var item in userAnswers)
+                {
+                    if (!_correctAnswers[_currentQuestionIndex].Contains(item)) return $"{_incorrect}\n{_wrongAnswers[_currentQuestionIndex]}";
+                }
+                _countOfCorrectAnswers++;
+                return _correct;
+            }
+            return $"{_incorrect}\n{_wrongAnswers[_currentQuestionIndex]}";
+        }
+
+        public string GetResult()
+        {
+            return $"Вы набрали {_countOfCorrectAnswers}/{CountOfQuestions}!";
+        }
+    }
+}
